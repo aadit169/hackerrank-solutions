887f14aecb03e32684ef6d9b2d4dc84d0963d690
aadit169 <aaditshah1258@gmail.com>
2026-08-21 21:21:32 +0530

    ✅ Input & Output - Character Analysis Tool [cpp] — approach1

---
+++ solutions/cpp/medium/input-output-character-analysis-tool-3-1/solution-approach1.cpp
@@ -0,0 +1,25 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/ku-uit-dsa-beginner/challenges/input-output-character-analysis-tool-3-1/problem?isFullScreen=true
+// Problem     Input & Output - Character Analysis Tool
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-21, 09:21 p.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    char ch;
+    cin >> ch;
+    cout << int(ch) << endl;
+    return 0;
+}
