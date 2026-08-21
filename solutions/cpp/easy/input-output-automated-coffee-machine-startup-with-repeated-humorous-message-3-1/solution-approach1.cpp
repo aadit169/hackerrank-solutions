8eddf0ef17800b4d493830e9bef01895e6b8d068
aadit169 <aaditshah1258@gmail.com>
2026-08-21 20:54:23 +0530

    ✅ Input & Output - Automated Coffee Machine Startup with Repeated Humorous Message [cpp] — approach1

---
+++ solutions/cpp/easy/input-output-automated-coffee-machine-startup-with-repeated-humorous-message-3-1/solution-approach1.cpp
@@ -0,0 +1,25 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/ku-uit-dsa-beginner/challenges/input-output-automated-coffee-machine-startup-with-repeated-humorous-message-3-1/problem?isFullScreen=true
+// Problem     Input & Output - Automated Coffee Machine Startup with Repeated Humorous Message
+// Difficulty  Easy
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-21, 08:54 p.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    cout<<"I need coffee too!!"<<endl;
+    cout<<"I need coffee too!!"<<endl;
+    cout<<"I need coffee too!!";
+    return 0;
+}
