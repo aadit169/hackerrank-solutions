4d774a2842cfef75803e9a9201a0e7da2ea74ca3
aadit169 <aaditshah1258@gmail.com>
2026-08-21 21:10:36 +0530

    ✅ Input & Output - User Registration System [cpp] — approach1

---
+++ solutions/cpp/medium/input-output-user-registration-system-3-1/solution-approach1.cpp
@@ -0,0 +1,29 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/ku-uit-dsa-beginner/challenges/input-output-user-registration-system-3-1/problem?isFullScreen=true
+// Problem     Input & Output - User Registration System
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-21, 09:10 p.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int age;
+    string name;
+    cin>>age;
+    cin>>name;
+    cout<<"User Details:"<<endl;
+    cout<<"Name: "<<name<<endl;
+    cout<<"Age: "<<age;
+    return 0;
+}
