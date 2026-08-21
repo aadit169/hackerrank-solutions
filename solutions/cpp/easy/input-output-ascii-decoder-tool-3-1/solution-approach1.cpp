049a04134e81aa64a01960dc1f9971d74e2803a3
aadit169 <aaditshah1258@gmail.com>
2026-08-21 21:28:08 +0530

    ✅ Input & Output - ASCII Decoder Tool [cpp] — approach1

---
+++ solutions/cpp/easy/input-output-ascii-decoder-tool-3-1/solution-approach1.cpp
@@ -0,0 +1,25 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/ku-uit-dsa-beginner/challenges/input-output-ascii-decoder-tool-3-1/problem?isFullScreen=true
+// Problem     Input & Output - ASCII Decoder Tool
+// Difficulty  Easy
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-21, 09:28 p.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<char(n);
+    return 0;
+}
