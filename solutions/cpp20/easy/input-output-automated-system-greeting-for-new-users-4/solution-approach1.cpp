2b3cdd14a96d52a3e8cd31020e43bf8de01b9d0a
aadit169 <aaditshah1258@gmail.com>
2026-08-21 20:50:50 +0530

    ✅ Input & Output - Automated System Greeting for New Users [cpp20] — approach1

---
+++ solutions/cpp20/easy/input-output-automated-system-greeting-for-new-users-4/solution-approach1.cpp
@@ -0,0 +1,23 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/ku-uit-dsa-beginner/challenges/input-output-automated-system-greeting-for-new-users-4/problem?isFullScreen=true
+// Problem     Input & Output - Automated System Greeting for New Users
+// Difficulty  Easy
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    cpp20
+// Status      Accepted
+// Submitted   2026-08-21, 08:50 p.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    cout<<"Hello, World!";
+    return 0;
+}
